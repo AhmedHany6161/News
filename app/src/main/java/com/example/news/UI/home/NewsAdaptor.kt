8aeda639b3dbdd230c76961225465baef521cd5d
MennaElhelaly <manonaelhelaly@gmail.com>
2021-04-03 17:03:13 +0200

    addHomeActivityAndadapters

--- app/src/main/java/com/example/news/UI/home/NewsAdaptor.kt
+++ app/src/main/java/com/example/news/UI/home/NewsAdaptor.kt
@@ -15,23 +15,23 @@ import kotlin.collections.ArrayList
 
 
 class NewsAdaptor(var news: ArrayList<News>) :
-    RecyclerView.Adapter<NewsAdaptor.HourlyViewHolder>() {
+    RecyclerView.Adapter<NewsAdaptor.NewsViewHolder>() {
     fun updateHours(newNew: List<News>) {
         news.clear()
         news.addAll(newNew)
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, p1: Int) = HourlyViewHolder(
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int) = NewsViewHolder(
         LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
     )
 
     override fun getItemCount() = news.size
-    override fun onBindViewHolder(holder: HourlyViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         holder.bind(news[position])
     }
 
-    class HourlyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    class NewsViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         private val title = view.findViewById<TextView>(R.id.title)
         private val date = view.findViewById<TextView>(R.id.date)
         private val auther = view.findViewById<TextView>(R.id.auther)
