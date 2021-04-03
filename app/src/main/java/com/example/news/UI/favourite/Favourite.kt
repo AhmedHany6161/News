bf3400f40e6de941d6bacf46c009803300024eb2
Esraa Fathy <fesraa240@gmail.com>
2021-04-03 15:44:11 +0200

    fav

---
+++ app/src/main/java/com/example/news/UI/favourite/Favourite.kt
@@ -0,0 +1,12 @@
+package com.example.news.UI.favourite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.news.R
+
+class Favourite : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favourite)
+    }
+}
