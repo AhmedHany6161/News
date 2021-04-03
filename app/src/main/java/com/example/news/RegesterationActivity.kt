eeeaf833a22048633c280775c594c51dd5742cdc
Esraa Fathy <fesraa240@gmail.com>
2021-04-03 11:11:50 +0200

    lottie lib

---
+++ app/src/main/java/com/example/news/RegesterationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegesterationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regesteration)
+    }
+}
