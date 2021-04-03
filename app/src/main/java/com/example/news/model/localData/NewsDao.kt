6d9c45d9448c2069b886fff14b70443387df61e6
ahmed <ahmed.hany.6161@gmail.com>
2021-04-03 16:29:47 +0200

    news Object

--- app/src/main/java/com/example/news/model/localData/NewsDao.kt
+++ app/src/main/java/com/example/news/model/localData/NewsDao.kt
@@ -3,12 +3,15 @@ package com.example.news.model.localData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import com.example.news.model.entitys.News
 import com.example.news.model.entitys.User
 import kotlinx.coroutines.flow.Flow
 @Dao
 interface NewsDao {
     @Insert
     suspend fun register(user: User)
+    @Insert
+    suspend fun addNews(news: News)
     @Query("select * from user where email==:sEmail And password=:sPassword ")
     suspend fun login(sEmail:String , sPassword:String): User?
 //    @Query("select * from user ")
